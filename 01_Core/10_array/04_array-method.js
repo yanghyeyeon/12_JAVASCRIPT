73efc545b590bea40f9e50751ee6455792f614be
yanghyeyeon <yhy4058@gmail.com>
2024-07-26 11:24:47 +0900

    core array method

--- 01_Core/10_array/04_array-method.js
+++ 01_Core/10_array/04_array-method.js
@@ -1,3 +1,37 @@
+// 배열 메소드
+
+const arr = [];
+
+// 배열의 생성자 함수 = Array
+console.log(arr.constructor === Array);
+
+// 배열의 프로토타입 객체 = Array.prototype
+console.log(Object.getPrototypeOf(arr) === Array.prototype);
+
+/* 
+Array.prototype.indexOf
+- indexOf : 배열에서 요소가 위치한 인덱스를 리턴
+- lastIndexOf : 배열의 요소가 위치한 마지막 인덱스를 리턴
+- includes : 배열에 해당 요소 포함 여부 리턴
+*/
+const foodList = ['물회', '삼계탕', '냉면', '수박', '물회'];
+
+console.log('--------------indexOf--------------');
+console.log(`foodList.indexOf('물회') : ${foodList.indexOf('물회')}`); //0
+// 중복값중 두번째 위치반환
+console.log(`foodList.indexOf('물회', 1) : ${foodList.indexOf('물회', 1)}`); // 4
+
+console.log(`foodList.indexOf('삼겹살') : ${foodList.indexOf('삼겹살')}`); // -1
+
+console.log('--------------LastIndexOf--------------');
+console.log(`foodList.lastIndexOf('물회') : ${foodList.lastIndexOf('물회')}`); // 4
+console.log(`foodList.lastIndexOf('물회', 1) : ${foodList.lastIndexOf('물회', 1)}`); // 0
+console.log(`foodList.lastIndexOf('삼겹살') : ${foodList.lastIndexOf('삼겹살')}`);  // -1
+
+console.log('--------------includes--------------');
+console.log(`foodList.includes('물회') : ${foodList.includes('물회')}`); // true
+console.log(`foodList.includes('삼겹살') : ${foodList.includes('삼겹살')}`); // false
+
 /* 
 Array.prototype.push : 배열의 맨 뒤에 요소를 추가
 Array.prototype.pop : 배열의 맨 뒤에 요소를 제거
@@ -67,6 +101,17 @@ console.log('--------------- slice ----------------');
 const front = ['HTML','CSS','JabaScript','jQuery'];
 
 // slice(시작 인덱스, 종료 인덱스)
-console.log(`front.slice(1,3) : ${front.slice(1,3)}`);
-console.log(`front : ${front}`);
+console.log(`front.slice(1,3) : ${front.slice(1,3)}`); // CSS,JabaScript
+console.log(`front : ${front}`); // HTML,CSS,JabaScript,jQuery
 
+console.log('---------------- join ------------------');
+// Array.prototype.join : 배열을 구분자로 결합하여 문자열로 반환
+
+const snackList = ['사탕','초콜릿','껌','과자']
+console.log(`snackList.join() : ${snackList.join()}`); // 구분자 ,
+console.log(`snackList.join() : ${snackList.join('/')}`); // 구분자 /
+
+console.log('--------------- reverse ---------------');
+
+// Array.prototype.reverse : 배열의 순서를 뒤집음
+console.log(`[1,2,3,4,5].reverse(): ${[1,2,3,4,5].reverse()}`);
