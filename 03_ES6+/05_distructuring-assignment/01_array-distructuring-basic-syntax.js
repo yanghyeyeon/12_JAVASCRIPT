f1b9a0135949f51eda2b7ad2ed4e0c631e8a1072
yanghyeyeon <yhy4058@gmail.com>
2024-08-16 15:10:29 +0900

    array distructuring basic syntax

---
+++ 03_ES6+/05_distructuring-assignment/01_array-distructuring-basic-syntax.js
@@ -0,0 +1,39 @@
+// 배열 구조 분해 할당
+// 구조 분해 할당을 사용해 배열을 변수로 '분해'해서 연결 할 수 있다.
+
+let nameArr = ["Gildong", "Hong"];
+
+// let [firstName, lastName] = nameArr;
+// let [firstName, lastName] = ["Gildong", "Hong"];
+// let firstName = nameArr[0];
+// let lastName = nameArr[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+// 반환값이 배열인 split 메서드
+let arr = "Saimdang Shin".split(' ');
+
+console.log(arr);
+
+let [firstName2, lastName2] = "Saimdang Shin".split(' ');
+
+console.log(firstName2);
+console.log(lastName2);
+
+
+// 쉼표를 사용해 필요하지 않은 배열 요소를 버릴 수 있다.
+
+let [firstName3, , lastName3] = ['fistName','middleName','lastName'];
+
+console.log(firstName3);
+console.log(lastName3);
+
+
+// 객체
+let user = {};
+
+[user.firstName, user.lastName] = 'Gwansoon Yu'.split(' ');
+
+console.log(user);
